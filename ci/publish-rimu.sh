774f13c86a4098d553d1c29f5ff0b12337371c49
Cameron Adams <cameron.adams@centrality.ai>
2019-03-12 12:43:22 +1300

    add scripts to deploy rimu separately

--- ci/publish-rimu.sh
+++ ci/publish-rimu.sh
@@ -12,16 +12,13 @@ GIT_SHORT="${GIT_COMMIT:0:8}"
 
 IMAGE_NAME="${IMAGE_NAME:-cennznet}"
 
-# remove 'origin/' from branch name and replace any '/' with '_'
-BRANCH="${GIT_BRANCH/origin\//}"
-BRANCH=${BRANCH/\//_}
-
-IMAGE_TAG="$BRANCH-$GIT_SHORT"
+IMAGE_TAG="rimu-$GIT_SHORT"
 
 # Azure container registry
 ACR_HOSTNAME=centralitycontainerregistry-on.azurecr.io
 
-FULL_IMAGE="${ACR_HOSTNAME}/centrality/${IMAGE_NAME}:${IMAGE_TAG}"
+RIMU_COMMIT="${ACR_HOSTNAME}/centrality/${IMAGE_NAME}:${IMAGE_TAG}"
+RIMU_LATEST="${ACR_HOSTNAME}/centrality/${IMAGE_NAME}:rimu"
 
 # GET IMAGE ID TO ADD TAG ON IT
 IMAGE_ID=$(docker images --format "{{.ID}}" "${IMAGE_NAME}")
@@ -29,11 +26,13 @@ IMAGE_ID=$(docker images --format "{{.ID}}" "${IMAGE_NAME}")
 docker login -u "${ACR_USR}" -p "${ACR_PSW}" "${ACR_HOSTNAME}"
 
 ## PUBLISH IMAGE TO Azure Container Repository
-echo "Uploading image: $FULL_IMAGE"
+echo "Uploading image: $RIMU_COMMIT and latest"
 
-docker tag "${IMAGE_ID}" "$FULL_IMAGE"
+docker tag "${IMAGE_ID}" "$RIMU_COMMIT"
+docker tag "${IMAGE_ID}" "$RIMU_LATEST"
 
-docker push "$FULL_IMAGE"
+docker push "$RIMU_COMMIT"
+docker push "$RIMU_LATEST"
 
 # Remove image after pushing to azure container registry
 docker rmi -f "${IMAGE_ID}"
